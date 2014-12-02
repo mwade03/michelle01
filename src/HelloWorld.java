d2b7a7fec4ba461b0c19466399b6c2eafd77e48c
Asif R Choudhury <choudhury.asif@gmail.com>
2014-12-01 18:17:58 -0600

    Added function

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,5 +4,11 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("dickspit");
+        System.out.println("howdy yo");
+        greet("bananas");
+    }
+
+    public static void greet(String name){
+        System.out.println("hi " + name);
     }
 }
