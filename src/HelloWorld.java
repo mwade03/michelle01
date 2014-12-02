04b327b438e97e73219504e1dd70c4d7aa2b292d
Asif R Choudhury <choudhury.asif@gmail.com>
2014-12-01 19:04:24 -0600

    Calculator

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -7,9 +7,13 @@ import java.io.InputStreamReader;
  */
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("what's your name");
-        String name = getInput();
-        System.out.println("hello " + name);
+//        System.out.println("what's your name");
+//        String name = getInput();
+//        System.out.println("hello " + name);
+        System.out.println("first number to add");
+        int number = Integer.parseInt(getInput());
+        int number2 = Integer.parseInt(getInput());
+        System.out.println(number + number2);
     }
 
 
